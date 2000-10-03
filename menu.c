48613fdb1498ce55c474baeb4ce84e592c2d72f5
Klaus Schmidinger <vdr@tvdr.de>
2000-10-03 14:06:44 +0200

    Implemented toggling between current and previous channel

--- menu.c
+++ menu.c
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: menu.c 1.29 2000/10/03 12:38:03 kls Exp $
+ * $Id: menu.c 1.30 2000/10/03 14:06:44 kls Exp $
  */
 
 #include "menu.h"
@@ -1186,7 +1186,7 @@ cDirectChannelSelect::~cDirectChannelSelect()
 eOSState cDirectChannelSelect::ProcessKey(eKeys Key)
 {
   switch (Key) {
-    case k0: case k1: case k2: case k3: case k4: case k5: case k6: case k7: case k8: case k9:
+    case k0 ... k9:
          if (number >= 0) {
             number = number * 10 + Key - k0;
             cChannel *channel = Channels.GetByNumber(number);
