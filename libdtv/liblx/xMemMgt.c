df5be87dc3aa8a94990dcc387729c551fb14a3db
Klaus Schmidinger <vdr@tvdr.de>
2002-08-24 15:42:45 +0200

    Added some missing #includes to files in libdtv for gcc 3.2

--- libdtv/liblx/xMemMgt.c
+++ libdtv/liblx/xMemMgt.c
@@ -26,6 +26,8 @@
 // Boston, MA 02111-1307, USA.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <malloc.h>
 
 #include "liblx.h"
--- libdtv/libsi/si_parser.c
+++ libdtv/libsi/si_parser.c
@@ -26,6 +26,7 @@
 // Boston, MA 02111-1307, USA.
 
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include <time.h>
 
