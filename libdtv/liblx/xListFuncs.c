848e79eec8026ed0836f64faad2148836be27836
Klaus Schmidinger <vdr@tvdr.de>
2003-05-11 14:42:36 +0200

    Changed C++ style comments in libdtv into C style to avoid warnings in gcc 3.x

--- libdtv/liblx/xListFuncs.c
+++ libdtv/liblx/xListFuncs.c
@@ -1,29 +1,30 @@
-//////////////////////////////////////////////////////////////
-///                                                        ///
-/// xListFuncs.c: list handling functions of liblx         ///
-///                                                        ///
-//////////////////////////////////////////////////////////////
-
-// $Revision: 1.1 $
-// $Date: 2001/06/25 12:29:47 $
-// $Author: hakenes $
-//
-//   (C) 1992-2001 Rolf Hakenes <hakenes@hippomi.de>, under the GNU GPL.
-//
-// liblx is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2, or (at your option)
-// any later version.
-//
-// liblx is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You may have received a copy of the GNU General Public License
-// along with liblx; see the file COPYING.  If not, write to the
-// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-// Boston, MA 02111-1307, USA.
+/*
+ *
+ * xListFuncs.c: list handling functions of liblx
+ *
+ *
+ * $Revision: 1.1 $
+ * $Date: 2001/08/15 10:00:00 $
+ * $Author: hakenes $
+ *
+ *   (C) 1992-2001 Rolf Hakenes <hakenes@hippomi.de>, under the GNU GPL.
+ *
+ * liblx is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * liblx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You may have received a copy of the GNU General Public License
+ * along with liblx; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
 
 #include <string.h>
 #include "liblx.h"
