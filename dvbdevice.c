f882ddf588f29f1154ac97fea7ba997ee718cb05
Klaus Schmidinger <vdr@tvdr.de>
2004-06-06 11:31:33 +0200

    Fixed missing audio after replaying a DVD

--- dvbdevice.c
+++ dvbdevice.c
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: dvbdevice.c 1.88 2004/06/05 16:33:35 kls Exp $
+ * $Id: dvbdevice.c 1.89 2004/06/06 11:28:28 kls Exp $
  */
 
 #include "dvbdevice.h"
@@ -767,6 +767,8 @@ bool cDvbDevice::SetChannelDevice(const cChannel *Channel, bool LiveView)
      if (IsPrimaryDevice())
         AddPid(Channel->Tpid(), ptTeletext);
      CHECK(ioctl(fd_audio, AUDIO_SET_AV_SYNC, true));
+     CHECK(ioctl(fd_audio, AUDIO_SET_MUTE, false));
+     CHECK(ioctl(fd_video, VIDEO_SET_BLANK, false));
      }
   else if (StartTransferMode)
      cControl::Launch(new cTransferControl(this, Channel->Vpid(), Channel->Apid1(), Channel->Apid2(), Channel->Dpid1(), Channel->Dpid2()));
