aec92c1334c29c3f2c94f87c41a4dd5cc7dca0f9
Klaus Schmidinger <vdr@tvdr.de>
2007-01-26 13:14:47 +0100

    Fixed i18n characters for the Hungarian texts

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: config.h 1.282 2007/01/07 13:45:19 kls Exp $
+ * $Id: config.h 1.282.1.1 2007/01/26 13:14:47 kls Exp $
  */
 
 #ifndef __CONFIG_H
@@ -21,7 +21,7 @@
 
 // VDR's own version number:
 
-#define VDRVERSION  "1.4.5"
+#define VDRVERSION  "1.4.5-1"
 #define VDRVERSNUM   10405  // Version * 10000 + Major * 100 + Minor
 
 // The plugin API's version number:
