c6ef96a6aea24e64c70792d7eec9e5f9cc89821a
Klaus Schmidinger <vdr@tvdr.de>
2007-10-14 10:09:08 +0200

    Made the default copy ctor and the assign operator of cRecording private

--- recording.h
+++ recording.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: recording.h 1.57 2007/06/17 12:53:05 kls Exp $
+ * $Id: recording.h 1.58 2007/10/14 10:06:48 kls Exp $
  */
 
 #ifndef __RECORDING_H
@@ -72,6 +72,8 @@ private:
   mutable char *name;
   mutable int fileSizeMB;
   cRecordingInfo *info;
+  cRecording(const cRecording&); // can't copy cRecording
+  cRecording &operator=(const cRecording &); // can't assign cRecording
   static char *StripEpisodeName(char *s);
   char *SortName(void) const;
   int GetResume(void) const;
