a97119d0b44b52b098de0aa4a4f38939bc4bba9b
Klaus Schmidinger <vdr@tvdr.de>
2007-11-25 13:35:08 +0100

    Fixed the declaration of cSubtitleObject::Decode8BppCodeString()

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: config.h 1.301 2007/11/10 13:38:19 kls Exp $
+ * $Id: config.h 1.302 2007/11/25 13:34:41 kls Exp $
  */
 
 #ifndef __CONFIG_H
@@ -22,13 +22,13 @@
 
 // VDR's own version number:
 
-#define VDRVERSION  "1.5.12"
-#define VDRVERSNUM   10512  // Version * 10000 + Major * 100 + Minor
+#define VDRVERSION  "1.5.13"
+#define VDRVERSNUM   10513  // Version * 10000 + Major * 100 + Minor
 
 // The plugin API's version number:
 
-#define APIVERSION  "1.5.12"
-#define APIVERSNUM   10512  // Version * 10000 + Major * 100 + Minor
+#define APIVERSION  "1.5.13"
+#define APIVERSNUM   10513  // Version * 10000 + Major * 100 + Minor
 
 // When loading plugins, VDR searches them by their APIVERSION, which
 // may be smaller than VDRVERSION in case there have been no changes to
