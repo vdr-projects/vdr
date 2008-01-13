29871d8585713d16fde2f1f8677c966e5ec7b101
Klaus Schmidinger <vdr@tvdr.de>
2008-01-13 12:23:39 +0100

    Fixed error handling in cCuttingThread::Action()

--- cutter.c
+++ cutter.c
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: cutter.c 1.17 2007/08/25 10:33:18 kls Exp $
+ * $Id: cutter.c 1.18 2008/01/13 12:22:21 kls Exp $
  */
 
 #include "cutter.h"
@@ -111,7 +111,9 @@ void cCuttingThread::Action(void)
                  }
               }
            else {
-              error = "index";
+              // Error, unless we're past the last cut-in and there's no cut-out
+              if (Mark || LastMark)
+                 error = "index";
               break;
               }
 
