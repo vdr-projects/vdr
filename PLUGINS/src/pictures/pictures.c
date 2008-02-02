538cbdb26a7804408c368804def0dce0dc4821c9
Klaus Schmidinger <vdr@tvdr.de>
2008-02-02 11:34:43 +0100

    Added option -i to the pictures plugin's pic2mpg to ignore unknown file types

--- PLUGINS/src/pictures/pictures.c
+++ PLUGINS/src/pictures/pictures.c
@@ -3,7 +3,7 @@
  *
  * See the README file for copyright information and how to reach the author.
  *
- * $Id: pictures.c 1.3 2008/01/27 15:44:58 kls Exp $
+ * $Id: pictures.c 1.4 2008/02/02 11:32:50 kls Exp $
  */
 
 #include <getopt.h>
@@ -11,7 +11,7 @@
 #include "menu.h"
 #include "player.h"
 
-static const char *VERSION       = "0.0.3";
+static const char *VERSION       = "0.0.4";
 static const char *DESCRIPTION   = trNOOP("A simple picture viewer");
 static const char *MAINMENUENTRY = trNOOP("Pictures");
 
