505574fa983b12ae872bb46f01c8782e3a15f5e1
Klaus Schmidinger <vdr@tvdr.de>
2010-08-29 13:40:37 +0200

    Added locking to the cCutter functions to avoid a crash in case CutRecording() is called from a plugin

--- cutter.h
+++ cutter.h
@@ -4,16 +4,19 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: cutter.h 2.1 2010/01/02 12:09:54 kls Exp $
+ * $Id: cutter.h 2.2 2010/08/29 13:32:33 kls Exp $
  */
 
 #ifndef __CUTTER_H
 #define __CUTTER_H
 
+#include "thread.h"
+
 class cCuttingThread;
 
 class cCutter {
 private:
+  static cMutex mutex;
   static char *editedVersionName;
   static cCuttingThread *cuttingThread;
   static bool error;
