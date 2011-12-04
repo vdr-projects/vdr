96647c3c3ddb0161bc9fe67007897daadeb4a26f
Klaus Schmidinger <vdr@tvdr.de>
2011-12-04 21:46:30 +0100

    Removed the '.pl' suffix from svdrpsend.pl

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: config.h 2.36 2011/12/03 14:19:52 kls Exp $
+ * $Id: config.h 2.37 2011/12/04 21:46:30 kls Exp $
  */
 
 #ifndef __CONFIG_H
@@ -22,13 +22,13 @@
 
 // VDR's own version number:
 
-#define VDRVERSION  "1.7.22"
-#define VDRVERSNUM   10722  // Version * 10000 + Major * 100 + Minor
+#define VDRVERSION  "1.7.23"
+#define VDRVERSNUM   10723  // Version * 10000 + Major * 100 + Minor
 
 // The plugin API's version number:
 
-#define APIVERSION  "1.7.22"
-#define APIVERSNUM   10722  // Version * 10000 + Major * 100 + Minor
+#define APIVERSION  "1.7.23"
+#define APIVERSNUM   10723  // Version * 10000 + Major * 100 + Minor
 
 // When loading plugins, VDR searches them by their APIVERSION, which
 // may be smaller than VDRVERSION in case there have been no changes to
