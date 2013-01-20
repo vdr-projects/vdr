1ba20fa20f632ffb548f57ca9b3b5b9bab5296d0
Klaus Schmidinger <vdr@tvdr.de>
2013-01-20 13:40:30 +0100

    Reduced the number of retries in cTransfer::Receive() to avoid blocking recordings in case the primary device can't handle the current live signal

--- transfer.h
+++ transfer.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: transfer.h 2.2 2010/01/29 16:38:09 kls Exp $
+ * $Id: transfer.h 2.3 2013/01/20 13:12:38 kls Exp $
  */
 
 #ifndef __TRANSFER_H
@@ -13,10 +13,13 @@
 #include "player.h"
 #include "receiver.h"
 #include "remux.h"
+#include "tools.h"
 
 class cTransfer : public cReceiver, public cPlayer {
 private:
   cPatPmtGenerator patPmtGenerator;
+  bool retriesExceeded;
+  cTimeMs timer;
 protected:
   virtual void Activate(bool On);
   virtual void Receive(uchar *Data, int Length);
