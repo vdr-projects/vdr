da404e00c0c95978bd2b126b6e2a6b01556630ec
Klaus Schmidinger <vdr@tvdr.de>
2014-03-10 12:12:44 +0100

    Fixed drawing the live indicator in the LCARS skin in case there are no devices

--- skinlcars.c
+++ skinlcars.c
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: skinlcars.c 2.21.1.3 2013/11/16 13:28:19 kls Exp $
+ * $Id: skinlcars.c 2.21.1.4 2014/03/10 12:12:19 kls Exp $
  */
 
 // "Star Trek: The Next Generation"(R) is a registered trademark of Paramount Pictures,
@@ -1298,7 +1298,7 @@ void cSkinLCARSDisplayMenu::DrawLiveIndicator(void)
   if (initial || y != lastLiveIndicatorY || Transferring != lastLiveIndicatorTransferring) {
      if (lastLiveIndicatorY >= 0)
         osd->DrawRectangle(xs12, lastLiveIndicatorY, xs13 - 1, lastLiveIndicatorY + lineHeight - 1, Theme.Color(clrBackground));
-     if (y >= 0) {
+     if (y > 0) {
         tColor ColorBg = Theme.Color(clrChannelFrameBg);
         osd->DrawRectangle(xs12, y, xs12 + lineHeight / 2 - 1, y + lineHeight - 1, ColorBg);
         osd->DrawEllipse  (xs12 + lineHeight / 2, y, xs13 - 1, y + lineHeight - 1, ColorBg, 5);
