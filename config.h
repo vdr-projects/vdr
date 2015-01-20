70aa4e42472653055e2025a0c5de322de82ebfd4
Klaus Schmidinger <vdr@tvdr.de>
2015-01-20 09:06:54 +0100

    Updated the Italian OSD texts

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: config.h 3.10 2015/01/12 14:32:17 kls Exp $
+ * $Id: config.h 3.11 2015/01/20 09:05:53 kls Exp $
  */
 
 #ifndef __CONFIG_H
@@ -22,13 +22,13 @@
 
 // VDR's own version number:
 
-#define VDRVERSION  "2.1.7"
-#define VDRVERSNUM   20107  // Version * 10000 + Major * 100 + Minor
+#define VDRVERSION  "2.1.8"
+#define VDRVERSNUM   20108  // Version * 10000 + Major * 100 + Minor
 
 // The plugin API's version number:
 
-#define APIVERSION  "2.1.7"
-#define APIVERSNUM   20107  // Version * 10000 + Major * 100 + Minor
+#define APIVERSION  "2.1.8"
+#define APIVERSNUM   20108  // Version * 10000 + Major * 100 + Minor
 
 // When loading plugins, VDR searches them by their APIVERSION, which
 // may be smaller than VDRVERSION in case there have been no changes to
