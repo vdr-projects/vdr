74eff2016830c24ecd52ab25f0f8048ee590e9da
Klaus Schmidinger <Klaus (dot) Schmidinger (at) tvdr (dot) de>
2018-04-09 10:16:00 +0200

    vdr-2.3.9-fixbitmapscaling.diff
    https://www.vdr-portal.de/forum/index.php?thread/131711-vdr-2-3-9-fixes/&postID=1303966#post1303966
    
    Wenn man VDR in einem Fenster laufen lässt und Untertitel angezeigt werden, und man dieses Fenster sehr klein macht, kannes passieren, dass VDR mit einer "division by zero" abstürzt, weil die skalierte Bitmap zu klein wird.
    
    Beiliegender Patch fixt das.

--- osd.c
+++ osd.c
@@ -839,7 +839,9 @@ cBitmap *cBitmap::Scaled(double FactorX, double FactorY, bool AntiAlias) const
 {
   // Fixed point scaling code based on www.inversereality.org/files/bitmapscaling.pdf
   // by deltener@mindtremors.com
-  cBitmap *b = new cBitmap(int(round(Width() * FactorX)), int(round(Height() * FactorY)), Bpp(), X0(), Y0());
+  int w = max(1, int(round(Width() * FactorX)));
+  int h = max(1, int(round(Height() * FactorY)));
+  cBitmap *b = new cBitmap(w, h, Bpp(), X0(), Y0());
   int RatioX = (Width() << 16) / b->Width();
   int RatioY = (Height() << 16) / b->Height();
   if (!AntiAlias || FactorX <= 1.0 && FactorY <= 1.0) {
