98fa6206ac8aaef4864a4de4ac2f14d821a3bd55
Klaus Schmidinger <vdr@tvdr.de>
2021-04-20 14:25:33 +0200

    Version 2.5.3

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
  * See the main source file 'vdr.c' for copyright information and
  * how to reach the author.
  *
- * $Id: config.h 5.2 2021/04/06 10:04:24 kls Exp $
+ * $Id: config.h 5.3 2021/04/20 14:25:33 kls Exp $
  */
 
 #ifndef __CONFIG_H
@@ -22,13 +22,13 @@
 
 // VDR's own version number:
 
-#define VDRVERSION  "2.5.2"
-#define VDRVERSNUM   20502  // Version * 10000 + Major * 100 + Minor
+#define VDRVERSION  "2.5.3"
+#define VDRVERSNUM   20503  // Version * 10000 + Major * 100 + Minor
 
 // The plugin API's version number:
 
-#define APIVERSION  "2.5.2"
-#define APIVERSNUM   20502  // Version * 10000 + Major * 100 + Minor
+#define APIVERSION  "2.5.3"
+#define APIVERSNUM   20503  // Version * 10000 + Major * 100 + Minor
 
 // When loading plugins, VDR searches them by their APIVERSION, which
 // may be smaller than VDRVERSION in case there have been no changes to
